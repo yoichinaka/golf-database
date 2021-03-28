d6ea1671d54df836d9725363f12993eecee83409
yoichinaka <yoichinaka22@outlook.com>
2021-03-28 14:05:33 +0900

    heroku2nd

--- models.py
+++ models.py
@@ -5,10 +5,11 @@ import json
 from flask_migrate import Migrate
 
 #database_filename = "golf_test"
-database_filename = os.getenv('database_name')
+#database_filename = os.getenv('database_name')
+database_path = os.environ['DATABASE_URL']
 project_dir = os.path.dirname(os.path.abspath(__file__))
 #database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
-database_path = "postgres://{}/{}".format('localhost:5432', database_filename)
+#database_path = "postgres://{}/{}".format('localhost:5432', database_filename)
 
 db = SQLAlchemy()
 
@@ -130,83 +131,4 @@ class Score(db.Model):
     def update(self):
         db.session.commit()
 
-# class Drink(db.Model):
-#     # Autoincrementing, unique primary key
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     #id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
-#     # String Title
-#     title = Column(String(100), unique=True)
-#     # the ingredients blob - this stores a lazy json blob
-#     # the required datatype is
-#     #  [{'color': string, 'name':string, 'parts':number}]
-#     recipe = Column(String(180), nullable=False)
-
-#     '''
-#     short()
-#         short form representation of the Drink model
-#     '''
-
-#     def short(self):
-#         short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
-#         print(self.id, self.title)
-#         return {
-#             'id': self.id,
-#             'title': self.title,
-#             'recipe': short_recipe
-#         }
-
-#     '''
-#     long()
-#         long form representation of the Drink model
-#     '''
-
-#     def long(self):
-#         return {
-#             'id': self.id,
-#             'title': self.title,
-#             'recipe': json.loads(self.recipe)
-#         }
-
-#     '''
-#     insert()
-#         inserts a new model into a database
-#         the model must have a unique name
-#         the model must have a unique id or null id
-#         EXAMPLE
-#             drink = Drink(title=req_title, recipe=req_recipe)
-#             drink.insert()
-#     '''
-
-#     def insert(self):
-#         db.session.add(self)
-#         db.session.commit()
-
-#     '''
-#     delete()
-#         deletes a new model into a database
-#         the model must exist in the database
-#         EXAMPLE
-#             drink = Drink(title=req_title, recipe=req_recipe)
-#             drink.delete()
-#     '''
-
-#     def delete(self):
-#         db.session.delete(self)
-#         db.session.commit()
-
-#     '''
-#     update()
-#         updates a new model into a database
-#         the model must exist in the database
-#         EXAMPLE
-#             drink = Drink.query.filter(Drink.id == id).one_or_none()
-#             drink.title = 'Black Coffee'
-#             drink.update()
-#     '''
-
-#     def update(self):
-#         db.session.commit()
-
-#     def __repr__(self):
-#         return json.dumps(self.short())
