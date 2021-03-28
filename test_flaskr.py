6c512f69506e0bcaeee0618779b5fdf6492ac7a5
yoichinaka <yoichinaka22@outlook.com>
2021-03-28 20:13:07 +0900

    setupdb

--- test_flaskr.py
+++ test_flaskr.py
@@ -17,14 +17,8 @@ class GolfTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        setup_db(self.app)
+        #setup_db(self.app)
 
-        # # binds the app to the current context
-        # with self.app.app_context():
-        #     self.db = SQLAlchemy()
-        #     self.db.init_app(self.app)
-        #     # create all tables
-        #     self.db.create_all()
 
     def tearDown(self):
         """Executed after reach test"""
